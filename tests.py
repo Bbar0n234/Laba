70c0d350044bff0414a61f921d57700d159af782
unknown <vilnus1337@mail.ru>
2024-05-10 22:02:36 +0300

    fix tests.py & + windows for CI

--- tests.py
+++ tests.py
@@ -17,11 +17,11 @@ class MyTestCase(TestCase):
         self.assertEqual(random_str, binary_to_text(text_to_binary(random_str)))
 
     def test_khafre(self):
-        random_str = str(random.randint(1, 1000) )
-        random_key = str(random.randint(1000, 100000))
+        test_str = "Hello_I_am_Ilya_3234k"
+        test_key = "crypto_key"
         random_rounds = random.randint(8, 16)
 
-        enc_str = khafre_cipher(random_str, random_key, random_rounds)
-        dec_str = khafre_decipher(enc_str, random_key, random_rounds)
+        enc_str = khafre_cipher(test_str, test_key, random_rounds)
+        dec_str = khafre_decipher(enc_str, test_key, random_rounds)
 
-        self.assertEqual(random_str, dec_str)
+        self.assertEqual(test_str, dec_str)
