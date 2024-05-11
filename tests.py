d240264d26ffd2839cd900482e112a16414db5d4
unknown <vilnus1337@mail.ru>
2024-05-11 11:40:39 +0300

    Add Linter

--- tests.py
+++ tests.py
@@ -1,19 +1,33 @@
-from main import text_to_binary, binary_to_text, khafre_cipher, khafre_decipher
+'''Модуль для написания Юнит-тестов'''
 from unittest import TestCase
+from main import text_to_binary, binary_to_text, khafre_cipher, khafre_decipher
 
 
 class MyTestCase(TestCase):
+    """Класс для Юнит-тестов"""
     def test_text_to_binary(self):
+        """
+        :return:
+        """
         self.assertEqual(text_to_binary("3234"), "00110011001100100011001100110100")
 
     def test_binary_to_text(self):
+        """
+        :return:
+        """
         self.assertEqual(binary_to_text("00110011001100100011001100110100"), "3234")
 
     def test_text_to_text(self):
+        """
+        :return:
+        """
         test_str = "3234"
         self.assertEqual(test_str, binary_to_text(text_to_binary(test_str)))
 
     def test_khafre(self):
+        """
+        :return:
+        """
         test_str = "Hello_I_am_Ilya_3234k"
         test_key = "cryptokey"
         test_rounds = 8
