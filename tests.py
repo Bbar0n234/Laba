10704b2e541961602ad0a6febcbb9195a3244701
vilnus1337 <vilnus1337@mail.ru>
2024-05-15 22:55:13 +0300

    add docs to main

--- tests.py
+++ tests.py
@@ -7,13 +7,15 @@ class MyTestCase(TestCase):
     """Класс для Юнит-тестов"""
     def test_text_to_binary(self):
         """
-        :return:
+        :return: fndsnjklfknsdknsdf
         """
         self.assertEqual(text_to_binary("3234"), "00110011001100100011001100110100")
 
     def test_binary_to_text(self):
         """
-        :return:
+        :param a: 1234123
+        :param b: 1234123
+        :return: 1234123 1234123
         """
         self.assertEqual(binary_to_text("00110011001100100011001100110100"), "3234")
 
